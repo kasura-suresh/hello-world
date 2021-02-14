430c3b9a74448e6b19af5e775ec397df8af23a0c
Sureshkumar Vemuri <kasura.suresh@gmail.com>
2021-02-14 20:24:33 +0530

    added new line

--- hello_world.cpp
+++ hello_world.cpp
@@ -6,5 +6,6 @@ int main()
   cout << "Hello First Project on GitHub.." << endl;
   cout << "Now trying to configure Jenkins to work with this.." << endl;
   cout << "Now trying to test Jenkins with this.." << endl;
+  cout << "another demo line" << endl;
   return 0;
 }
